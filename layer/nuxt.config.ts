e18f283cd7037c704dc87dd63de19f4bffff9b32
hareland <packagist@proton.me>
2025-11-07 19:19:37 +0100

    Remove "docus" dependency and update `optimizeDeps` mapping in `nuxt.config.ts`.

--- layer/nuxt.config.ts
+++ layer/nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
         config.optimizeDeps.include ||= []
         config.optimizeDeps.include.push('@nuxt/content > slugify')
         config.optimizeDeps.include = config.optimizeDeps.include
-          .map(id => id.replace(/^@nuxt\/content > /, 'docus > @nuxt/content > '))
+          .map(id => id.replace(/^@nuxt\/content > /, 'simple-content-site > @nuxt/content > '))
       })
     },
   ],
