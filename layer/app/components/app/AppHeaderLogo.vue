f3443e22eb02a2209b372bb8d936109883642f17
hareland <packagist@proton.me>
2025-11-08 01:58:30 +0100

    feat: add props definition for class handling in `AppHeaderLogo.vue` and refine `class` binding using `$props` for improved reactivity

--- layer/app/components/app/AppHeaderLogo.vue
+++ layer/app/components/app/AppHeaderLogo.vue
@@ -3,6 +3,10 @@ import { useSiteHeader, twMerge } from '#imports'
 
 const appConfig = useAppConfig()
 const { data: header } = await useSiteHeader()
+
+defineProps<{
+  class?: string | string[]
+}>()
 </script>
 
 <template>
@@ -11,7 +15,7 @@ const { data: header } = await useSiteHeader()
     :light="header?.logo?.light || appConfig.header?.logo?.light || appConfig.header?.logo?.dark"
     :dark="(header?.logo?.dark || header?.logo?.light) || appConfig.header?.logo?.dark || appConfig.header?.logo?.light"
     :alt="header?.logo?.alt || appConfig.header?.logo?.alt || appConfig.header?.title"
-    :class="twMerge('h-6 w-auto shrink-0', $attrs.class || '')"
+    :class="twMerge('h-6 w-auto shrink-0', $props.class || '')"
   />
   <span v-else>
     {{ header?.title || appConfig.header?.title || '{appConfig.header.title}' }}
