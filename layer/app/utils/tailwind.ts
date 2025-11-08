7e73ee45c7e3313319af934a6aa413cc4a3dd086
hareland <packagist@proton.me>
2025-11-08 02:15:47 +0100

    refactor: adjust header logo classes and refine `AppHeader` layout logic

--- layer/app/utils/tailwind.ts
+++
@@ -1,6 +0,0 @@
-import type { ClassNameValue } from 'tailwind-merge'
-import { twMerge as _twMerge } from 'tailwind-merge'
-
-export const twMerge = (...classLists: ClassNameValue[]) => {
-  return _twMerge(classLists)
-}
