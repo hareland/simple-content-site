e5282679f8bf7aecd9cbe1b81ce849a9a3ce89fe
hareland <packagist@proton.me>
2026-01-19 14:13:26 +0100

    fix(i18n): prevent redirect for strategies without prefix and log strategy in dev mode

--- layer/app/plugins/i18n.ts
+++ layer/app/plugins/i18n.ts
@@ -6,9 +6,20 @@ export default defineNuxtPlugin(() => {
     return
   }
 
+  // todo: This could probably be injected by a module that has access to the nuxtApp.,
+  const withoutRedirect = ['no_prefix', 'prefix_except_default']
+  // @ts-expect-error This is not typed, but might need to move this config later.
+  if (i18nConfig.strategy && withoutRedirect.includes(i18nConfig.strategy)) {
+    return
+  }
+
   addRouteMiddleware((to) => {
     if (to.path === '/') {
-      const cookieLocale = useCookie('i18n_redirected').value || i18nConfig.defaultLocale || 'en'
+      if (import.meta.dev) {
+        // @ts-expect-error Typed expose strategy
+        console.log(`[I18N] Redirecting to default locale using ${i18nConfig.strategy}`)
+      }
+      const cookieLocale = useCookie('i18n_redirected').value || i18nConfig?.defaultLocale || 'en'
 
       return navigateTo(`/${cookieLocale}`)
     }
