b9ee985faf9279fa63700c803dbf25c012304faf
hareland <packagist@proton.me>
2026-03-08 21:29:32 +0100

    chore(config): add vitest configuration for Nuxt test environment

---
+++ vitest.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vitest/config'
+import { defineVitestProject } from '@nuxt/test-utils/config'
+import { fileURLToPath } from 'node:url'
+
+export default defineConfig({
+  test: {
+    projects: [
+      // {
+      //   test: {
+      //     name: 'unit',
+      //     include: ['test/unit/*.{test,spec}.ts'],
+      //     environment: 'node',
+      //   },
+      // },
+      // {
+      //   test: {
+      //     name: 'e2e',
+      //     include: ['test/e2e/*.{test,spec}.ts'],
+      //     environment: 'node',
+      //   },
+      // },
+      await defineVitestProject({
+        test: {
+          name: 'nuxt',
+          include: ['test/nuxt/*.{test,spec}.ts'],
+          environment: 'nuxt',
+          environmentOptions: {
+            nuxt: {
+              rootDir: fileURLToPath(new URL('./test/fixtures/i18n', import.meta.url)),
+            },
+          },
+        },
+      }),
+    ],
+  },
+})
