0be6543b7ee8179a300ab8c1011e5416f1cd6995
hareland <packagist@proton.me>
2026-03-08 21:28:32 +0100

    fix(modules): update css module to improve template handling and CSS array initialization

--- layer/modules/css.ts
+++ layer/modules/css.ts
@@ -2,32 +2,36 @@ import { defineNuxtModule, addTemplate, createResolver } from '@nuxt/kit'
 import { joinURL } from 'ufo'
 import { resolveModulePath } from 'exsolve'
 
-const resolver = createResolver(import.meta.url)
 export default defineNuxtModule({
   meta: {
     name: 'css',
   },
   async setup(_options, nuxt) {
     const dir = nuxt.options.rootDir
+    const resolver = createResolver(import.meta.url)
 
     const contentDir = joinURL(dir, 'content')
     const uiPath = resolveModulePath('@nuxt/ui', { from: import.meta.url, conditions: ['style'] })
     const tailwindPath = resolveModulePath('tailwindcss', { from: import.meta.url, conditions: ['style'] })
     const layerDir = resolver.resolve('../app')
+    // const assistantDir = resolver.resolve('../modules/assistant')
 
     const cssTemplate = addTemplate({
-      filename: 'website.css',
+      filename: 'simple-content-site.css',
       getContents: () => {
         return `@import ${JSON.stringify(tailwindPath)};
 @import ${JSON.stringify(uiPath)};
 
 @source "${contentDir.replace(/\\/g, '/')}/**/*";
 @source "${layerDir.replace(/\\/g, '/')}/**/*";
-@source "../../app.config.ts";`
+@source "../../app.config.ts";
+`
+        // @source "${assistantDir.replace(/\\/g, '/')}/**/*";
       },
     })
 
-    nuxt.options.css ||= []
-    nuxt.options.css.unshift(cssTemplate.dst)
+    if (Array.isArray(nuxt.options.css)) {
+      nuxt.options.css.unshift(cssTemplate.dst)
+    }
   },
 })
