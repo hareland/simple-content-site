ee527c9f0f24bb64293a189852cc60a755855789
hareland <packagist@proton.me>
2026-03-26 18:39:50 +0100

    chore(seo): remove unused `useSeoMeta` setup in `[[lang]]/[...slug].vue`

--- layer/app/pages/[[lang]]/[...slug].vue
+++ layer/app/pages/[[lang]]/[...slug].vue
@@ -46,11 +46,6 @@ const headline = ref(findPageHeadline(navigation?.value, page.value?.path))
 watch(() => navigation?.value, () => {
   headline.value = findPageHeadline(navigation?.value, page.value?.path) || headline.value
 })
-
-// todo: make the landing OG component customizable.
-if (page.value && page.value.seo) {
-  useSeoMeta(page.value.seo)
-}
 </script>
 
 <template>
